82aabac66766fc8875470e942d333f6d9fa01de9
Bhagya <bhagyanadukandy@gmail.com>
2021-03-15 23:45:35 +0530

    contact file try 3 using php

--- submit.php
+++ submit.php
@@ -1,11 +1,11 @@
 <?php
- $data_file = fopen("exap.txt", "a")
- $name = $_POST["name"])
- $email = $_POST["email"])
- $tel = $_POST["tel"])
- $msg = $_POST["msg"])
+ $data_file = fopen("exap.txt", "a");
+ $name = $_POST["name"];
+ $email = $_POST["email"];
+ $tel = $_POST["tel"];
+ $msg = $_POST["msg"];
  $text_to_write = $name . " " . $email . " " . $tel . " " . $msg ;
- fwrite($data_file,$text_to_write)
- fclose($data_file)
+ fwrite($data_file,$text_to_write);
+ fclose($data_file);
 
 ?>
